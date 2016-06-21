b7f88ac70787130988682b211a181f804c4f34e8
Louis Moreau <luis.omoreau@gmail.com>
2016-06-21 17:03:53 +0200

    added default files

---
+++ lcd/backlight.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- charset: utf-8 -*-
+# This is a port of https://github.com/Seeed-Studio/Grove_LCD_RGB_Backlight
+# (c) 2014 Helge Jung <helge.jung@youngage.eu>
+# Released unter the LGPL, like the original.
+
+from smbus import SMBus
+
+class Backlight(object):
+    REG_RED = 0x04 # pwm2
+    REG_GREEN = 0x03 # pwm1
+    REG_BLUE = 0x02 # pwm0
+
+    REG_MODE1 = 0x00
+    REG_MODE2 = 0x01
+    REG_OUTPUT = 0x08
+
+    def __init__(self, bus, address):
+        if not isinstance(bus, SMBus):
+            raise TypeError
+
+        self.bus = bus
+        self.address = int(address)
+
+        # initialize
+        self.set_register(self.REG_MODE1, 0)
+        self.set_register(self.REG_MODE2, 0)
+
+        # all LED control by PWM
+        self.set_register(self.REG_OUTPUT, 0xAA)
+
+    def set_register(self, addr, value):
+        self.bus.write_byte_data(self.address, addr, value)
+
+    def set_color(self, red, green, blue):
+        r = int(red)
+        g = int(green)
+        b = int(blue)
+        self.set_register(self.REG_RED, r)
+        self.set_register(self.REG_GREEN, g)
+        self.set_register(self.REG_BLUE, b)
+
+if __name__ == '__main__':
+    import time
+
+    bus = SMBus(1)
+    light = Backlight(bus, 0x62)
+
+    light.set_color(255, 0, 0)
+    time.sleep(1)
+    light.set_color(0,255,0)
+    time.sleep(1)
+    light.set_color(0,0,255)
+    time.sleep(1)
+    light.set_color(255,0,255)
+    time.sleep(1)
+    light.set_color(255,255,255)
+    time.sleep(1)
+    light.set_color(0, 0, 0)
---
+++ opencv/videostream_demo.py
@@ -0,0 +1,166 @@
+# USAGE
+# python videostream_demo.py
+# python videostream_demo.py --picamera 1
+
+# import the necessary packages
+from imutils.video import VideoStream
+from smbus import SMBus
+import datetime
+import argparse
+import imutils
+import time
+import cv2
+import config
+import face
+import backlight
+import screen
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--picamera", type=int, default=1,
+        help="whether or not the Raspberry Pi camera should be used")
+args = vars(ap.parse_args())
+
+# initialize the video stream and allow the cammera sensor to warmup
+vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
+time.sleep(2.0)
+
+class Display(object):
+    backlight = None
+    screen = None
+
+    def __init__(self, bus):
+        self.backlight = backlight.Backlight(bus, 0x62)
+        self.screen    = screen.Screen(bus, 0x3e)
+
+    def write(self, text):
+        self.screen.write(text)
+
+    def color(self, r, g, b):
+        self.backlight.set_color(r, g, b)
+
+    def move(self, col, row):
+        self.screen.setCursor(col, row)
+
+class Backlight(object):
+    REG_RED = 0x04 # pwm2
+    REG_GREEN = 0x03 # pwm1
+    REG_BLUE = 0x02 # pwm0
+
+    REG_MODE1 = 0x00
+    REG_MODE2 = 0x01
+    REG_OUTPUT = 0x08
+
+    def __init__(self, bus, address):
+        if not isinstance(bus, SMBus):
+            raise TypeError
+
+        self.bus = bus
+        self.address = int(address)
+
+        # initialize
+        self.set_register(self.REG_MODE1, 0)
+        self.set_register(self.REG_MODE2, 0)
+
+        # all LED control by PWM
+        self.set_register(self.REG_OUTPUT, 0xAA)
+
+    def set_register(self, addr, value):
+        self.bus.write_byte_data(self.address, addr, value)
+
+    def set_color(self, red, green, blue):
+        r = int(red)
+        g = int(green)
+        b = int(blue)
+        self.set_register(self.REG_RED, r)
+        self.set_register(self.REG_GREEN, g)
+        self.set_register(self.REG_BLUE, b)
+
+d = Display(SMBus(1))
+d.move(0, 0)
+light = Backlight(SMBus(1), 0x62)
+light.set_color(0, 0, 0)
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+print 'Loading training data...'
+
+d.write("Setting up")
+model = cv2.createEigenFaceRecognizer()
+model.load(config.TRAINING_FILE)
+
+print 'Training data loaded!'
+d.write("Ready")
+
+# loop over the frames from the video stream
+while True:
+        # grab the frame from the threaded video stream and resize it
+        # to have a maximum width of 400 pixels
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.3,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+                # Crop and resize image to face.
+                crop = face.resize(face.crop(gray, x, y, w, h))
+                # Test face against model.
+                label, confidence = model.predict(crop)
+                
+                #print 'Predicted {0} face with confidence {1} (lower is more confident).'.format(
+                #        'POSITIVE' if label == config.POSITIVE_LABEL else 'NEGATIVE',
+                #        confidence)
+                print 'Predicted {0} face with confidence {1} (lower is more confident).'.format(label, confidence)
+                if label == config.LOUIS_LABEL and confidence < 2600:
+                        #print 'Recognized face!'
+                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                        light.set_color(0, 255, 0)
+                        d.move(0, 0)
+                        d.write("Hello Louis".format(label))
+                elif label == config.ANTHO_LABEL and confidence < 2600:
+                        #print 'Recognized face!'
+                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                        light.set_color(0, 255, 0)
+                        d.move(0, 0)
+                        d.write("Hello Antho".format(label))
+                elif label == config.NICO_LABEL and confidence < 2600:
+                        #print 'Recognized face!'
+                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                        light.set_color(0, 255, 0)
+                        d.move(0, 0)
+                        d.write("Hello Nico".format(label))
+                elif label == config.BATISTE_LABEL and confidence < 2200:
+                        #print 'Recognized face!'
+                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                        light.set_color(0, 255, 0)
+                        d.move(0, 0)
+                        d.write("Hello Batiste".format(label))
+                else:
+                        #print 'Did not recognize face!'
+                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+                        light.set_color(0, 0, 0)
+                        d.move(0, 0)
+                        #d.write("Do I know you?")
+                
+
+        # show the frame
+        d.write("                   ")
+        d.write("                   ")
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+                light.set_color(0, 0, 0)
+                break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
